d65e278d3d686bf7ccc65b41723b79b0d52ff152
AleksandrKhv <sidby83@gmail.com>
2021-10-24 16:49:56 +0200

    79%

--- main.js
+++ main.js
@@ -579,6 +579,7 @@ var myDog = {
 myDog["name"]='Happy Coder'*/
 
 
+/*
 // Setup
 var myDog = {
     "name": "Happy Coder",
@@ -587,4 +588,23 @@ var myDog = {
     "friends": ["freeCodeCamp Campers"],
     "bark": "woof"
 };
-delete myDog.tails
+delete myDog.tails*/
+
+
+// Setup
+function phoneticLookup(val) {
+    var result = ""
+    // Only change code below this line
+    let lookup = {
+        "alpha": "Adams",
+        "bravo":  "Boston",
+        "charlie":  "Chicago",
+        "delta":  "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank",
+    }
+    result = lookup[val]
+    // Only change code above this line
+    return result;
+}
+phoneticLookup("charlie");
