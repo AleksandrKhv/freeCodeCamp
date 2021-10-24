7e1f656762f7a0b27166c173c26d898c149963f2
AleksandrKhv <sidby83@gmail.com>
2021-10-24 12:59:44 +0200

    codewar3

--- codewars.js
+++ codewars.js
@@ -63,6 +63,7 @@ Examples
 Constraints
 0 <= input.length <= 100*/
 
+/*
 function validParentheses(parens){
     var n = 0;
     for (var i = 0; i < parens.length; i++) {
@@ -72,4 +73,25 @@ function validParentheses(parens){
     }
 
     return n == 0;
+}*/
+// ---------------------------------------------------
+/*
+iq test
+Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+
+! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+
+Examples:
+    iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+
+iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd*/
+
+
+function iqTest(numbers){
+    let initNumber = numbers.split(' ').map(val=>val*1)
+    let eventNumber = initNumber.filter(val=>val%2===0)
+    let addNumber =initNumber.filter(val=>val%2===1)
+    return eventNumber.length > addNumber.length ?
+        initNumber.indexOf(addNumber[0])+1 :
+        initNumber.indexOf(eventNumber[0])+1
 }
