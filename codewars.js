1c3ac2f35f96f203f20817c5387dc9ff52b71ece
AleksandrKhv <sidby83@gmail.com>
2021-10-25 20:02:32 +0200

    CW12

--- codewars.js
+++ codewars.js
@@ -324,6 +324,7 @@ The code would be called like so:
 // result should == "apples, pears\ngrapes\nbananas"*/
 
 
+/*
 function solution(input, markers) {
     let arr = input.split('\n');
     let mark;
@@ -334,4 +335,33 @@ function solution(input, markers) {
         arr[i] = arr[i].slice(0, mark - 1);
     }
     return arr.join('\n');
-};
+};*/
+
+// =====================================
+
+/*
+Your job is to write a function which increments a string, to create a new string.
+
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+Examples:
+
+foo -> foo1
+
+foobar23 -> foobar24
+
+foo0042 -> foo0043
+
+foo9 -> foo10
+
+foo099 -> foo100
+
+Attention: If the number has leading zeros the amount of digits should be considered.*/
+
+function incrementString (strng) {
+    if(isNaN(parseInt(strng[strng.length - 1]))) return strng + '1';
+    return strng.replace(/(0*)([0-9]+$)/, function(match, p1, p2) {
+        var up = parseInt(p2) + 1;
+        return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+    })
+}
