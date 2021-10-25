8e3d5ac5758991ca60ecd66c4acc8f2b11146681
AleksandrKhv <sidby83@gmail.com>
2021-10-25 19:25:25 +0200

    CW8

--- codewars.js
+++ codewars.js
@@ -159,6 +159,7 @@ function isPangram(string){
 }*/
 
 // ===========================================
+/*
 
 Welcome.
 
@@ -181,4 +182,32 @@ function alphabetPosition(text) {
     }
 
     return result.slice(0, result.length - 1);
+}*/
+// ====================================
+
+/*
+Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+    Example 1:
+a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns ["arp", "live", "strong"]
+
+Example 2:
+a1 = ["tarp", "mice", "bull"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns []
+
+Notes:
+    Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+    In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+    Beware: r must be without duplicates.*/
+
+function inArray(array1,array2){
+    let arrR = array2.join(" ")
+    return array1.filter(item => arrR.search(item) !== -1 && item !== undefined ).sort()
 }
