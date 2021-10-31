92c3c8f0187500f7dcc855bc1c321cb5d2dbc908
AleksandrKhv <sidby83@gmail.com>
2021-10-31 08:48:28 +0100

    22

--- codewars.js
+++ codewars.js
@@ -457,8 +457,26 @@ function orderWeight(strng) {
 
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
+/*
 function boolToWord( bool ){
     if (bool === true) {
         return 'Yes'
     } return 'No'
+}*/
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/*
+Convert a Number to a String!
+We need a function that can transform a number into a string.
+
+    What ways of achieving this do you know?
+
+    Examples:
+    123 --> "123"
+999 --> "999"*/
+
+function numberToString(num) {
+
+    return num = num.toString();
+
 }
