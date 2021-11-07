7143c4ef7980d5f3f5a28c804c431416333dbf14
AleksandrKhv <sidby83@gmail.com>
2021-11-07 12:12:42 +0100

    27

--- codewars.js
+++ codewars.js
@@ -547,6 +547,24 @@ time = 6.7---> litres = 3
 time = 11.8--> litres = 5*/
 
 
+/*
 function litres(time) {
     return Math.floor(time/2);
+}*/
+
+// ==========================================
+
+// Get Nth Even Number
+
+/*
+Return the Nth Even Number
+
+Example(Input --> Output)
+1 --> 0 (the first even number is 0)
+3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+100 --> 198
+1298734 --> 2597466*/
+
+function nthEven(n){
+    return n*2 -2
 }
