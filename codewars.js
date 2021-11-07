a44a606797788d7f5d6fd5688bfd31c9f04902b8
AleksandrKhv <sidby83@gmail.com>
2021-11-07 12:07:12 +0100

    26

--- codewars.js
+++ codewars.js
@@ -525,6 +525,28 @@ doubleChar("apple");*/
     Вам дается один параметр - исходная строка.
     Вам не нужно беспокоиться о строках, содержащих менее двух символов.*/
 
+/*
 function removeChar(str){
     return str.slice(1, -1)
-};
+};*/
+
+/*
+Keep Hydrated!
+Nathan loves cycling.
+
+    Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+    You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+    For example:
+
+    time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres = 5*/
+
+
+function litres(time) {
+    return Math.floor(time/2);
+}
